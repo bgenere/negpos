3bcb18c5d357c1f545a87a6aec51fc32cc0ff1fc
bgenere <bgenere@users.noreply.github.com>
2015-09-09 08:58:29 +0200

    Removed html declarations unused

--- themes/negpos/404.php
+++ themes/negpos/404.php
@@ -5,62 +5,50 @@
 if (!defined('WEBPATH')) die();
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<!-- cl ajout favicon jpg-->
-<link rel="icon" type="image/jpeg" href="http://negpos.fr/negposphoto/uploaded/images/favicon.jpg" />
+<html>
+	<head>
+		<!-- cl ajout favicon jpg-->
+		<link rel="icon" type="image/jpeg" href="http://negpos.fr/negposphoto/uploaded/images/favicon.jpg" />
+	
+		<meta charset="<?php echo LOCAL_CHARSET; ?>">
+		<?php zp_apply_filter('theme_head'); ?>
+		<?php printHeadTitle(); ?>
+		<link rel="stylesheet" href="<?php echo $_zp_themeroot; ?>/style.css" type="text/css" />
+	</head>
 
-	<?php zp_apply_filter('theme_head'); ?>
-	<title><?php echo getBareGalleryTitle(); ?> | <?php echo gettext("Object not found"); ?></title>
-	<meta http-equiv="content-type" content="text/html; charset=<?php echo LOCAL_CHARSET; ?>" />
-	<link rel="stylesheet" href="<?php echo $_zp_themeroot; ?>/style.css" type="text/css" />
-</head>
-
-<body>
-<?php zp_apply_filter('theme_body_open'); ?>
-
-<div id="main">
-
-	<div id="header">
-		<h1><?php echo getGalleryTitle();?></h1>
-		</div>
-
-		<div id="content">
-		<div id="breadcrumb">
-	<h2><a href="<?php echo getGalleryIndexURL(); ?>">Index</a> &raquo; <strong><?php echo gettext("Object not found"); ?></strong></h2>
-	</div>
-
-	<div id="content-error">
-
-		<div class="errorbox">
- 		<?php
-		echo gettext("The Zenphoto object you are requesting cannot be found.");
-		if (isset($album)) {
-			echo '<br />'.gettext("Album").': '.html_encode($album);
-		}
-		if (isset($image)) {
-			echo '<br />'.gettext("Image").': '.html_encode($image);
-		}
-		if (isset($obj)) {
-			echo '<br />'.gettext("Theme page").': '.html_encode(substr(basename($obj),0,-4));
-		}
- 		?>
-		</div>
-
-</div>
-
-
-
-<div id="footer">
-<?php include("footer.php"); ?>
-</div>
-
-
-
-</div><!-- content -->
-
-</div><!-- main -->
-<?php zp_apply_filter('theme_body_close');?>
-</body>
+	<body>
+		<?php zp_apply_filter('theme_body_open'); ?>
+		
+		<div id="main">
+		
+			<div id="header">
+				<h1 class="title">
+					<a href="<?php echo html_encode(getGalleryIndexURL()); ?>" title="<?php echo gettext('Gallery Index'); ?>"><?php echo html_encode(getGalleryTitle()); ?></a>
+				</h1>
+			</div>
+		
+			<div id="content">
+				<div id="breadcrumb">
+					<h2><?php printGalleryIndexURL(); ?></h2>
+				</div>
+		
+				<div id="content-error">
+		
+					<div class="errorbox">
+						<?php print404status(isset($album) ? $album : NULL, isset($image) ? $image : NULL, $obj); ?>
+					</div>
+		
+				</div>
+				
+				<div id="footer">
+					<?php include("footer.php"); ?>
+				</div>
+		
+			</div><!-- content -->
+		
+		</div><!-- main -->
+		<?php 
+			zp_apply_filter('theme_body_close');
+		?>
+	</body>
 </html>
