93a4337e47e091cf68340231c24b35344c94ac1e
bgenere <bgenere@users.noreply.github.com>
2015-11-04 17:06:50 +0100

    moving slider images

--- themes/negpos/carousel.php
+++ themes/negpos/carousel.php
@@ -1,15 +1,24 @@
+ <!-- Get Files in thème/images/slider -->
+ <?php
+	$path = $_zp_themeroot."/images/slider/";
+	$filepattern = $path."*.jpg";
+	$files = glob($filepattern);
+    //Show files
+	echo $files ."<br/>";
+?> 
+ 
  <!-- Carousel - begin -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
 	<!-- Wrapper for slides -->
 	<div class="carousel-inner">
 		<div class="item active">
-			<img src="<?php echo $_zp_themeroot;?>/images/headbanner.jpg" >		
+			<img src="<?php echo $_zp_themeroot;?>/images/slider/headbanner.jpg" >		
 		</div>
 		<div class="item">
-			<img src="<?php echo $_zp_themeroot;?>/images/headbanner2.jpg" >
+			<img src="<?php echo $_zp_themeroot;?>/images/slider/headbanner2.jpg" >
 		</div>
 		<div class="item">
-			<img src="<?php echo $_zp_themeroot;?>/images/headbanner3.jpg" >
+			<img src="<?php echo $_zp_themeroot;?>/images/slider/headbanner3.jpg" >
 		</div>
 	</div>
 
