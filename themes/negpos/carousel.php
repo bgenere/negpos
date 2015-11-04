3ad92080d49b9e76fafdce2942385ce65afd1f02
bgenere <bgenere@users.noreply.github.com>
2015-11-04 15:59:08 +0100

    Simplification du Slider page d'accueil

--- themes/negpos/carousel.php
+++ themes/negpos/carousel.php
@@ -1,31 +1,15 @@
  <!-- Carousel - begin -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
-	<!-- Indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-	</ol>
-
 	<!-- Wrapper for slides -->
 	<div class="carousel-inner">
 		<div class="item active">
 			<img src="<?php echo $_zp_themeroot;?>/images/headbanner.jpg" >		
-			<div class="carousel-caption">
-				<h2>NEGPOS PHOTOGRAPHIE</h2>
-			</div>
 		</div>
 		<div class="item">
 			<img src="<?php echo $_zp_themeroot;?>/images/headbanner2.jpg" >
-			<div class="carousel-caption">
-				<h2>FORMATIONS</h2>
-			</div>
 		</div>
 		<div class="item">
 			<img src="<?php echo $_zp_themeroot;?>/images/headbanner3.jpg" >
-			<div class="carousel-caption">
-				<h2>EXPOSITIONS</h2>
-			</div>
 		</div>
 	</div>
 
